9fd26f0ac608915ff9622405b36832130aa83f2d
mdavie <marianahodniki1991@gmail.com>
2022-03-21 21:26:49 -0600

    read me added

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # The-Mentalist
 This application is a guessing game, where the program chooses a random number and we will try to get it right
+
+
+## Technologies 
+
+* JavaScript
+* HTML
+* CSS
+
+
+## GitHub Repository
+
+* [The Mentalist](https://github.com/mhdavie/The-Mentalist)
+
+## GitHub Pages
+
+* [The Mentalist]()
+
+
+
+
+## Application Screenshot
+
+![](./images/002.png)
--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 //the number will be inteiro 
 // Math.random is a js method that will help our game to have a random number
 // and we multply by 11 so we can get 0 -- 10
+
 var secretNumber = parseInt(Math.random() * 11);
 
 function Chutar() {
+    var elementResultado = document.getElementById("resultado")
     var chute = parseInt(document.getElementById("valor").value);
+    if (chute == secretNumber) {
+        elementResultado.innerHTML = "You got it! Wow!"        
+    }else if (chute > 10 || chute < 0 ){
+        elementResultado.innerHTML = "You need to insert a number between 0 and 10. Pay attention, Pal"
+    }else {
+        elementResultado.innerHTML = "Wrong :( the secret number was " + secretNumber;
+    }
 }
